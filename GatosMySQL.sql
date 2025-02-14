750f4d898e08f1719590a4dcb3fbaf592beb6e79
Roberto Gómez <a25959@svalero.com>
2025-02-14 15:40:36 +0100

    Añadidos un par de gatos de bigotes

--- GatosMySQL.sql
+++ GatosMySQL.sql
@@ -52,12 +52,21 @@ CREATE TABLE Gato (
 
 INSERT INTO Gato (Id_Protectora, Nombre_Gato, Raza, Edad, Esterilizado, Sexo, Descripcion_Gato, Imagen_Gato)
 VALUES 
-(1, 'Widow', 'Pardo', 4, 0, 'Macho', 'Al haber vivido mucho tiempo en la calle es algo desconfiado. Necesita que le den su espacio para no sentirse amenazado.', '/Images/Widow.png'),
-(2, 'Claudia', 'Gris', 1, 1, 'Hembra', 'Tiene el típico carácter de nu gato, cercana pero cuando ella quiere.', '/Images/Claudia.png'),
-(1, 'Sira', 'Pardo', 1, 1, 'Hembra', 'Es una gata que se encontró en un polígono y al principio es un poco tímida pero con un poco de paciencia es muy cariñosa.', '/Images/Sira.png'),
-(1, 'Milu', 'Tuxedo', 7, 1, 'Macho', 'Es muy bueno.', '/Images/Milu.png'),
-(1, 'Lupita', 'Blanca', 1, 1, 'Hembra', 'Necesita una familia con paciencia, tiene muchos miedos y necesita tiempo para volver a confiar.', '/Images/Lupita.png'),
-(2, 'Charlotte', 'Tuxedo', 1, 1, 'Hembra', 'Es muy buena y un amor.', '/Images/Charlotte.png');
+(2, 'Widow', 'Pardo', 4, 0, 'Macho', 'Al haber vivido mucho tiempo en la calle es algo desconfiado. Necesita que le den su espacio para no sentirse amenazado.', '/Images/Widow.png'),
+(2, 'Claudia', 'Gris', 1, 1, 'Hembra', 'Tiene el típico carácter de un gato, cercana pero cuando ella quiere.', '/Images/Claudia.png'),
+(2, 'Sira', 'Pardo', 1, 1, 'Hembra', 'Es una gata que se encontró en un polígono y al principio es un poco tímida pero con un poco de paciencia es muy cariñosa.', '/Images/Sira.png'),
+(2, 'Milu', 'Tuxedo', 7, 1, 'Macho', 'Es muy bueno.', '/Images/Milu.png'),
+(2, 'Lupita', 'Blanca', 1, 1, 'Hembra', 'Necesita una familia con paciencia, tiene muchos miedos y necesita tiempo para volver a confiar.', '/Images/Lupita.png'),
+(2, 'Charlotte', 'Tuxedo', 1, 1, 'Hembra', 'Es muy buena y un amor.', '/Images/Charlotte.png'),
+(1, 'Tulipa', 'Tuxedo', 1, 1, 'Hembra', 'Es muy enérgica y le gusta mucho jugar con alguien a todas horas.', '/Images/Tulipa.png'),
+(1, 'Vix', 'Siamés', 1, 1, 'Macho', 'Es muy cariñoso y siempre quiere jugar.', '/Images/Vix.png'),
+(1, 'Daisy', 'Pardo', 100, 1, 'Hembra', 'Sin info.', '/Images/Daisy.png'),
+(1, 'Ryu', 'Pardo', 3, 1, 'Hembra', 'Es algo distante, no le gusta que se le echen encima y necesita su espacio.', '/Images/Ryu.png'),
+(1, 'Blanqui', 'Blanco y negro', 100, 1, 'Macho', 'Sin info.', '/Images/Blanqui.png'),
+(1, 'Cookie', 'Negro', 100, 1, 'Macho', 'Sin info.', '/Images/Cookie.png'),
+(1, 'Kika', 'Pardo', 100, 1, 'Hembra', 'Sin info.', '/Images/Kika.png'),
+(1, 'Sol', 'Naranja y blanco', 100, 1, 'Macho', 'Sin info.', '/Images/Sol.png');
+
 
 SELECT * FROM Gato;
 
